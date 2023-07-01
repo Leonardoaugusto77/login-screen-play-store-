cf2991d7bb26049ccf6a779ae2d1462130ab03c3
Heisen <leonardopjh30@gmail.com>
2023-07-01 07:41:29 -0300

    feat(style): created the styles for the form and slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,46 @@ export default function App() {
     <>
       <div className="container_main">
         <div className="form_section">
-          <h1>Create an account</h1>
-          <p>and get access to the latest releases with unique discounts</p>
+          <div className="title_form">
+            <h1>Create an account</h1>
+            <p>and get access to the latest releases with unique discounts</p>
+          </div>
 
           <form>
             <div>
-              <input type="text" />
               <span>Name</span>
+              <input type="text" />
             </div>
             <div>
-              <input type="email" />
               <span>Email</span>
+              <input type="email" />
             </div>
             <div>
-              <input type="password" />
               <span>Password</span>
+              <input type="password" />
+            </div>
+
+            <div className="action_buttons_section">
+              <div>
+                <button>Create account</button>
+              </div>
+
+              <div>
+                <span>ICON_GOOGLE</span>
+                <button>Sign up with Google</button>
+              </div>
+              <div className="text_forgetPassword">
+                <p>
+                  Already have an account? Log in <strong>Log in</strong>
+                </p>
+              </div>
             </div>
           </form>
         </div>
 
-        <div className="action_buttons_section">
-          <div>
-            <button>Create account</button>
-          </div>
-
-          <div>
-            <span>ICON_GOOGLE</span>
-            <button>Sign up with Google</button>
-          </div>
-          <div className="text_forgetPassword">
-            <p>
-              Already have an account? Log in <strong>Log in</strong>
-            </p>
-          </div>
+        <div className="slider_section">
+          <Slider />
         </div>
-
-        <Slider />
       </div>
     </>
   );
