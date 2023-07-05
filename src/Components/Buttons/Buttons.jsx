f3d5c517d83b4942deb59bad851b4b7e80c74a18
Heisen <leonardopjh30@gmail.com>
2023-07-04 21:47:45 -0300

    feat(slider): created functions and test

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -1,9 +1,9 @@
-export default function Buttons({ Text, logo: Logo }) {
+export default function Buttons({ Text, logo: Logo, Action }) {
   return (
     <>
       <div className="action_buttons_section">
         <div className="create_account_button">
-          <button>
+          <button onClick={Action}>
             {Logo && <Logo size={30} style={{ marginRight: "5px" }} />}
             <span>{Text}</span>
           </button>
