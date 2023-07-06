220fe5c2b1b8cb9e1efebcd522ee21773bc8360c
Heisen <leonardopjh30@gmail.com>
2023-07-06 08:43:35 -0300

    feat(slider): created functions responsibly from next and previuos imgs

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 export default function Slider({ RequestApi }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNext = () => {
+    const nextIndex = (currentImageIndex + 1) % RequestApi.length;
+    setCurrentImageIndex(nextIndex);
+  };
+
+  const handlePrevious = () => {
+    const previousIndex =
+      (currentImageIndex - 1 + RequestApi.length) % RequestApi.length;
+    setCurrentImageIndex(previousIndex);
+  };
+
   return (
-    <>
-      <div className="slider_section">
-        {RequestApi.map((item, index) => (
-          <div className="slider_item" key={index}>
-            <h3> {item.name} </h3>
-            <img src={item.src} alt="Imagens dos jogos" />
-          </div>
-        ))}
+    <div className="slider_section">
+      <div className="slider_item">
+        <img
+          src={RequestApi[currentImageIndex].src}
+          alt={RequestApi[currentImageIndex].name}
+        />
+        <h3>{RequestApi[currentImageIndex].name}</h3>
       </div>
-    </>
+      <div className="slider_buttons">
+        <button onClick={handlePrevious}>Voltar</button>
+        <button onClick={handleNext}>Próximo</button>
+      </div>
+    </div>
   );
 }
